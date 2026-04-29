0f7f17ee33957bea200509e9f8bedf40259b1b88
Ryan Forsyth <forsyth2@llnl.gov>
2026-04-28 17:31:18 -0700

    Updates for May newsletter

--- e3sm_comms/newsletter_reviewer/main.py
+++ e3sm_comms/newsletter_reviewer/main.py
@@ -6,11 +6,12 @@ from e3sm_comms.utils import IO_DIR
 def main():
     c = Config("newsletter")
     c.file_input_story_versions = (
-        f"{IO_DIR}/input/newsletter_reviewer/2026_02_newsletter.csv"
+        f"{IO_DIR}/input/newsletter_reviewer/2026_05_newsletter.csv"
     )
     c.sensitive_terms_file = f"{IO_DIR}/input/shared/sensitive_terms.txt"
     c.first_person_file = f"{IO_DIR}/input/newsletter_reviewer/first_person_ok_urls.txt"
-    c.newsletter_test_link = "https://us18.campaign-archive.com/?e=__test_email__&u=11f9e1f9713b9366390852682&id=f5a2221a36"
+    # c.newsletter_test_link = "https://us18.campaign-archive.com/?e=__test_email__&u=11f9e1f9713b9366390852682&id=f5a2221a36"
+    c.newsletter_test_link = ""
     c.output_dir = f"{IO_DIR}/output/newsletter_reviewer/"  # Must end with "/"
     c.requested_output = ["newsletter_review_table"]
     c.check_links_work = True
