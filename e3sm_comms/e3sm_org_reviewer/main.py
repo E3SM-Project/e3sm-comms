d5ef25131545769ec04292a6eefbc80b99fe170a
Ryan Forsyth <forsyth2@llnl.gov>
2026-05-04 11:35:48 -0700

    Update print lines in e3sm org reviewer

--- e3sm_comms/e3sm_org_reviewer/main.py
+++ e3sm_comms/e3sm_org_reviewer/main.py
@@ -22,7 +22,7 @@ def main():
     with open(INPUT_SEARCH_PHRASES, "r", encoding="utf-8") as f:
         terms: List[str] = [line.rstrip("\n").lower() for line in f]
         list_search_phrases: List[str] = sorted(terms)
-    print(f"Checking {len(list_input_e3sm_org_paths)} e3sm.org pages")
+    print(f"Checking {len(list_input_e3sm_org_paths)} accessible e3sm.org pages")
     links = LinkedURLs(
         list_input_e3sm_org_paths,
         scan_links_for_sensitive_terms=True,
@@ -36,6 +36,7 @@ def main():
     # Check that the inaccessible pages are in fact inaccessible ###############
     with open(INPUT_ARCHIVED_E3SM_ORG_PATHS, "r", encoding="utf-8") as f:
         list_input_archived_e3sm_org_paths: List[str] = [line.strip() for line in f]
+    print(f"Checking {len(list_input_archived_e3sm_org_paths)} archived e3sm.org pages")
     with open(OUTPUT_INCORRECTLY_ACCESSIBLE_E3SM_ORG_PATHS, "w", encoding="utf-8") as f:
         for e3sm_url in list_input_archived_e3sm_org_paths:
             e3sm_url_status = get_e3sm_url_status(e3sm_url)
